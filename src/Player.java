43455b3c1039d1e7d79c27c14403a38320ed5c79
nemeth.balint.mark.2 <nemeth.balint.mark.2022i@bankitatabanya.hu>
2025-10-15 12:52:32 +0200

    Player

---
+++ src/Player.java
@@ -0,0 +1,41 @@
+import java.util.ArrayList;
+
+public class Player {
+    private Scene scene;
+    private ArrayList<Item> Inventory;
+
+    public Player(Scene startingScene) {
+        this.scene = startingScene;
+        this.Inventory = new ArrayList<>();
+    }
+
+    public Scene getScene() {
+        return this.scene;
+    }
+
+    public void setScene(Scene scene) {
+        this.scene = scene;
+    }
+
+    public void takeItem(Item item) {
+        this.Inventory.add(item);
+    }
+
+    public String getInventoryString() {
+        if (this.Inventory.isEmpty()) {
+            return "A leltárad üres.";
+        }
+
+        // A StringBuilder objektumot olyan szituációkban használjuk,
+        // mint például egy loop, ahol sok stringet fűzünk össze (concat)
+        // Ez azért szükséges, mert a Java-ban a String immutable,
+        // és egyébként mindig újjat hozna létre, ami költséges és lassú
+        StringBuilder returnString = new StringBuilder();
+
+        for (Item item : this.Inventory) {
+            returnString.append(" ").append(item.getName());
+        }
+
+        return returnString.toString();
+    }
+}
